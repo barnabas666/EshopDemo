a54772a482e41a34638faa1a0330bedffb2dfed0
barnabas666 <barnabas666@email.cz>
2024-06-21 18:20:41 +0200

    Add Starting EshopComponent
    Add EshopComponent and setup basic routing for this Component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EshopComponent } from './eshop/eshop.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: EshopComponent }
+];
