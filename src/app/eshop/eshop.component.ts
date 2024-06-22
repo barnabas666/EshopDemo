c195763d2ae15307eb043f57f966b434980f5e18
barnabas666 <barnabas666@email.cz>
2024-06-22 11:50:51 +0200

    Add Starting EshopComponent again
    Add EshopComponent and setup basic routing for this Component
    hopefully for last time.

---
+++ src/app/eshop/eshop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eshop',
+  standalone: true,
+  imports: [],
+  templateUrl: './eshop.component.html',
+  styleUrl: './eshop.component.scss',
+})
+export class EshopComponent {
+  title = "Barny's Eshop";
+}
