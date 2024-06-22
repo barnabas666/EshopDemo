e227212d677cade5a8bf2e873b86915cf06fa283
barnabas666 <barnabas666@email.cz>
2024-06-22 11:31:46 +0200

    Revert "Add Starting EshopComponent"
    This reverts commit a54772a482e41a34638faa1a0330bedffb2dfed0.
    
    Just want changes in this commit to be not only at local branch
    but at remote too. So I will repeat process in this commit
    again with new branch pushed to remote repo.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { EshopComponent } from './eshop/eshop.component';
 
-export const routes: Routes = [
-    { path: '', component: EshopComponent }
-];
+export const routes: Routes = [];
