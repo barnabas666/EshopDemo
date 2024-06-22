fa47582e2d8113599e15405dcfdc8cd6d5136622
barnabas666 <barnabas666@email.cz>
2024-06-22 17:52:55 +0200

    Add sample products.
    Add sample products with images inside assets folder

--- src/app/eshop/eshop.component.ts
+++ src/app/eshop/eshop.component.ts
@@ -9,4 +9,43 @@ import { Component } from '@angular/core';
 })
 export class EshopComponent {
   title = "Barny's Eshop";
+
+  items = [
+    {
+      id: 1,
+      name: 'Black Hoodie',
+      image: 'assets/images/products/image1.jpg',
+      price: '21',
+    },
+    {
+      id: 2,
+      name: 'Branded Shoes',
+      image: 'assets/images/products/image2.jpg',
+      price: '13.5',
+    },
+    {
+      id: 3,
+      name: 'White',
+      image: 'assets/images/products/image3.jpg',
+      price: '69.0',
+    },
+    {
+      id: 4,
+      name: 'Gray Dress 1',
+      image: 'assets/images/products/image4.jpg',
+      price: '315',
+    },
+    {
+      id: 5,
+      name: 'Black T-Shirt (Mens)',
+      image: 'assets/images/products/image5.jpg',
+      price: '55.0',
+    },
+    {
+      id: 6,
+      name: 'Jeans Jacket',
+      image: 'assets/images/products/image6.jpg',
+      price: '115.0',
+    },
+  ];
 }
