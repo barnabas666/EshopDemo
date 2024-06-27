a6ce3907f125ebef0fe7d05e5fb2fa20ddc4617f
barnabas666 <barnabas666@email.cz>
2024-06-27 18:25:43 +0200

    Create POST, PUT and DELETE requests part 2
    After EshopComponent we added those requests
    in ServerService too.

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Options } from '../models/models';
+import { Options, Product } from '../models/models';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +15,26 @@ export class ServerService {
   // we must convert our result to Observable of T (T will be PaginatorProducts)
   get<T>(url: string, options: Options): Observable<T> {
     return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
+
+  // Used to make a POST request to the API
+  // we are invoking HttpClient, calling POST method of T, passing url, Product and Options inside request,
+  // we must convert our result to Observable of T (T will be PaginatorProducts)
+  post<T>(url: string, body: Product, options: Options): Observable<T> {
+    return this.httpClient.post<T>(url, body, options) as Observable<T>;
+  }
+
+  // Used to make a PUT request to the API
+  // we are invoking HttpClient, calling PUT method of T, passing url, Product and Options inside request,
+  // we must convert our result to Observable of T (T will be PaginatorProducts)
+  put<T>(url: string, body: Product, options: Options): Observable<T> {
+    return this.httpClient.put<T>(url, body, options) as Observable<T>;
+  }
+
+  // Used to make a DELETE request to the API
+  // we are invoking HttpClient, calling DELETE method of T, passing url and Options inside request,
+  // we must convert our result to Observable of T
+  delete<T>(url: string, options: Options): Observable<T> {
+    return this.httpClient.delete<T>(url, options) as Observable<T>;
   }
 }
